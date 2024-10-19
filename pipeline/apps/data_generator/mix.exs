acbffc6630f3e43a6ab75fc22b67867d4884e0c3
Eugene Simonov <es@madappgang.com>
2024-10-19 19:53:13 +0300

    Install emqx/emqtt in data generator and collector

--- pipeline/apps/data_generator/mix.exs
+++ pipeline/apps/data_generator/mix.exs
@@ -28,6 +28,7 @@ defmodule DataGenerator.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:emqtt, github: "emqx/emqtt", tag: "1.13.2", system_env: [{"BUILD_WITHOUT_QUIC", "1"}]}
     ]
   end
 end
