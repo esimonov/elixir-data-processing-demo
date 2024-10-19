8a6fe707c5eac962460a55950f24dc66c9ef8379
Eugene Simonov <es@madappgang.com>
2024-10-19 12:33:55 +0300

    Create data generator (mix new data_generator --app data_generator)

---
+++ pipeline/data_generator/lib/data_generator.ex
@@ -0,0 +1,18 @@
+defmodule DataGenerator do
+  @moduledoc """
+  Documentation for `DataGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DataGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
