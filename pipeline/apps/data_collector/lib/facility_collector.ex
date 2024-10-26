d64908c25bb90533e4a966b288a30cf2893ed166
Eugene Simonov <es@madappgang.com>
2024-10-26 13:18:07 +0300

    Schedule periodically

--- pipeline/apps/data_collector/lib/facility_collector.ex
+++ pipeline/apps/data_collector/lib/facility_collector.ex
@@ -8,12 +8,7 @@ defmodule FacilityCollector do
   defp via_tuple(facility_id), do: {:via, Registry, {Registry.Facilities, facility_id}}
 
   def init(_args) do
-    state = %{
-      timer: nil,
-      measurements: %{}
-    }
-
-    {:ok, schedule_aggregation(state)}
+    {:ok, reset_state()}
   end
 
   def handle_cast({:measurement, signal_name, %{ts: _ts, val: value}}, state) do
@@ -24,34 +19,33 @@ defmodule FacilityCollector do
 
   def handle_info(:aggregate, state) do
     compose_aggregated_document(Map.get(state, :measurements, %{}))
+
+    {:noreply, reset_state()}
   end
 
   defp update_measurement(state, signal_name, value) do
     IO.puts("Signal: #{signal_name}, val: #{value}")
 
-    measurements_state = Map.get(state, :measurements, %{})
-
-    signal_state = Map.get(measurements_state, signal_name, %{sum: 0.0, count: 0})
+    signal_state = Map.get(state.measurements, signal_name, %{sum: 0.0, count: 0})
 
     updated_signal_state = %{
       sum: signal_state.sum + value,
       count: signal_state.count + 1
     }
 
-    updated_measurements_state = Map.put(measurements_state, signal_name, updated_signal_state)
+    updated_measurements_state = Map.put(state.measurements, signal_name, updated_signal_state)
 
     %{state | measurements: updated_measurements_state}
   end
 
   defp compose_aggregated_document(measurements_state) do
     IO.puts(Jason.encode!(measurements_state))
-
-    {:noreply, schedule_aggregation(%{:measurements => %{}, timer: nil})}
   end
 
-  defp schedule_aggregation(state) do
-    timer = Process.send_after(self(), :aggregate, 10_000)
-
-    %{state | timer: timer}
+  defp reset_state() do
+    %{
+      measurements: %{},
+      timer: Process.send_after(self(), :aggregate, 10_000)
+    }
   end
 end
